59d9e6b167181b44a51717b42618a7f1a0290469
tahta d. timur <tahtadaritimoer@gmail.com>
2017-06-30 00:48:07 +0700

    fix jupyter config

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,14 @@
+import os
+from IPython.lib import passwd
+
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.mathjax_url = 'nbextensions/mathjax/MathJax.js'
+c.NotebookApp.open_browser = False
+c.NotebookApp.disable_check_xsrf = True
+c.MultiKernelManager.default_kernel_name = 'sagemath'
+
+# sets a password if PASSWORD is set in the environment
+if 'PASSWORD' in os.environ:
+  c.NotebookApp.password = passwd(os.environ['PASSWORD'])
+  del os.environ['PASSWORD']
