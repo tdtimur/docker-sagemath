793961163b987c692b55e4dd6a6fc4a8891156fa
tdtimur <tahtadaritimoer@gmail.com>
2017-07-12 16:42:01 +0700

    fix jupyter

--- jupyter.sh
+++ jupyter.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-rm /home/sage/SageMath/local/etc/jupyter/jupyter_notebook_config.py && \
-cp /home/sage/jupyter_notebook_config.py /home/sage/SageMath/local/etc/jupyter/ && \
 sage --notebook=jupyter --notebook-dir=/home/sage/jupyter/
